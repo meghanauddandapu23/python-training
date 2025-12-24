92bbd73e675e8331a7067cde926c9e394424ddcc
meghanauddandapu23 <meghauddandapu12@gmail.com>
2025-12-24 10:53:47 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*",end="")
+    print()
