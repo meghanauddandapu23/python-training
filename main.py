abbdc5b6ded4918144a3abcfabe7d9960896de7e
meghanauddandapu23 <meghauddandapu12@gmail.com>
2025-12-24 10:11:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+x=int(input())
+for i in range (1,x+1):
+    if (i%3==0) and (i%5==0):
+        print("Fizz buzz")
+    elif i%5==0:
+        print("buzz")
+    elif(i%3==0):
+        print("fizz")
+    else:
+        print(i)
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
