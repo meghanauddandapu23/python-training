acadf142614e08b7753794abfb5135b4f4e87222
meghanauddandapu23 <meghauddandapu12@gmail.com>
2025-12-26 15:18:24 +0530

    Add files via upload

---
+++ halopyramid.py
@@ -0,0 +1,17 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+n=int(input())
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(2*i+1):
+        if j==0 or i==n-1 or j==2*i:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
