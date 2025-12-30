4713a73168b43d4382fb4fc092203906d880ad6e
meghanauddandapu23 <meghauddandapu12@gmail.com>
2025-12-30 13:59:26 +0530

    Add files via upload

---
+++ methodoverride.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+class MethodOverride1:
+    def display(self):
+        print("method involved from base class")
+class MethodOverride2:
+    def display(self):
+        print("Method involved from the derived class")
+ob=MethodOverride2()
+ob.display()
