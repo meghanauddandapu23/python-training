883e871964bb7212af4fa215666b6dcec2394d60
meghanauddandapu23 <meghauddandapu12@gmail.com>
2025-12-30 13:54:05 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def fact(n):
+    f=1 
+    for i in range(1,n+1):
+        f*=i  
+    print(f)
+fact(5)
